d0a3eec3caa67b1733a64da530065af58fe9e982
Nagriar <nagriar@nagriar-Latitude-E6400>
2012-04-18 14:22:50 +0200

    Add sqlmanager class

---
+++ src/SqlManager/SqlManager.hh
@@ -0,0 +1,20 @@
+/*
+ * SqlManager.hh
+ *
+ *  Created on: 18 avr. 2012
+ *      Author: nagriar
+ */
+
+#ifndef SQLMANAGER_HH_
+#define SQLMANAGER_HH_
+
+class SqlManager
+{
+  public:
+    SqlManager();
+    virtual ~SqlManager();
+  public:
+
+};
+
+#endif /* SQLMANAGER_HH_ */
