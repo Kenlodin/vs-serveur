5eb48c0606179deab392e1023dab23902be4ad2c
Nagriar <nagriar@nagriar-Latitude-E6400>
2012-04-21 00:06:17 +0200

    Merged

--- src/main.cc
+++ src/main.cc
@@ -14,8 +14,7 @@
 #include "Thread/ThreadPool.hh"
 #include "SqlManager/SqlManager.hh"
 
-int
-main ()
+int main()
 {
   //	Network* network = new Network(36000, 36001);
   //	Diffusion* diffusion = new Diffusion();
@@ -24,23 +23,14 @@ main ()
 
   //  tools::Thread t();
   //  t.run ();
-<<<<<<< HEAD
-    ThreadPool<Diffusion> t (5);
-    ThreadPool<Tracker> t2 (5);
-//  SqlManager::getInstance ().connect ();
-//  SqlManager::getInstance ().getThreeServers ();
-//  SqlManager::getInstance ().addServer ("55.22.33.44", 1234);
-//  SqlManager::getInstance ().getThreeServers ();
-
-=======
-
+  ThreadPool<Diffusion> t (5);
+  ThreadPool<Tracker> t2(5);
   SqlManager::getInstance ().connect ();
   //  SqlManager::getInstance ().getThreeServers ();
   //  SqlManager::getInstance ().addServer ("55.22.33.44", 1234);
   //  SqlManager::getInstance ().getThreeServers ();
   //  SqlManager::getInstance ().addClient (5, "88.88.8.88", "127.0.0.1", 150, "azasa5z51dsq1");
   //  SqlManager::getInstance ().addClient ("test", "0fd7bf4aef2dbe4e71ac2cc93e75ed02d9931822", "87.9.152.32", "192.168.0.1", 150);
->>>>>>> f6d66baba92517874cc479c51cc612f644f7a4ca
   return 0;
 }
 
