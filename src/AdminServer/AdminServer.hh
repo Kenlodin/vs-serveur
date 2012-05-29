01f5af8cf6432eb520a1fad866e2ddcad16a7766
aymeric <aymeric@localhost>
2012-05-29 15:08:13 +0200

    Ajout de commentaire dans AdminServer.hh

--- src/AdminServer/AdminServer.hh
+++ src/AdminServer/AdminServer.hh
@@ -27,9 +27,20 @@ private:
   virtual ~AdminServer();
 
 public:
+  /**
+   * Retourne une unique instance de la classe AdminServer
+   * @return 
+   */
   static AdminServer& getInstance();
 
 public:
+  /**
+   * 
+   * @param code
+   * @param packet
+   * @param sock
+   * @return 
+   */
   int routing(unsigned int code, sf::Packet& packet, sf::SocketTCP& sock);
 
 private:
@@ -37,13 +48,65 @@ private:
   const handler route_[AS::MAX_VALUE];
 
 private:
+  /**
+   * 
+   * @param sender
+   * @param packet
+   * @return 
+   */
   int send(sf::SocketTCP& sender, sf::Packet& packet);
 
 private:
+  ///
+  /// L'ensemble des méthodes ci-dessous sont de type AdminServeur lui 
+  /// demandant d'effectuer une action
+  ///
+  
+  /**
+   * Elle coupe donc le serveur (close socket client, ...)
+   * 
+   * @param packet Le packet reçus
+   * @param sock Le socket
+   * @return Si tout c'est passé correctement
+   */
   int asShutdown(sf::Packet& packet, sf::SocketTCP& sock);
+  
+  /**
+   * Suppression de tous les clients connectées au serveur.
+   * 
+   * @param packet Le packet reçus
+   * @param sock Le socket
+   * @return 
+   */
   int asClear(sf::Packet& packet, sf::SocketTCP& sock);
+  
+  /**
+   * Le serveur va télécharger via le lien d'origine (HTTP) la vidéo. Cette fonction
+   * est généralement appellée lors de l'ajout de la vidéo une unique fois sur
+   * un seul serveur
+   * 
+   * @param packet Le packet reçus
+   * @param sock Le socket
+   * @return 
+   */
   int asDownloadOrig(sf::Packet& packet, sf::SocketTCP& sock);
+  
+  /**
+   * Le serveur va télécharger une vidéo sur un autre serveur.
+   * 
+   * @param packet Le packet reçus
+   * @param sock Le socket
+   * @return 
+   */
   int asDownload(sf::Packet& packet, sf::SocketTCP& sock);
+  
+  /**
+   * Déconnecte un client du serveur.
+   * 
+   * @param packet Le packet reçus
+   * @param sock Le socket
+   * @return 
+   */
   int asRemove(sf::Packet& packet, sf::SocketTCP& sock);
 
 
