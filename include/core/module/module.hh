0228ac3e5a58638aab3927fcf0561e87bc554988
Corentin derbois <corentin@lse.epita.fr>
2012-07-17 12:24:30 -0400

    Add lot preprocessor macro to manage API

--- include/core/module/module.hh
+++ include/core/module/module.hh
@@ -17,6 +17,24 @@
 // Librairie Includes
 # include <dlfcn.h>
 
+
+# define SECTION_INFO_STR ".modinfo"
+
+# define __STRINGIFY(STR)  #STR
+# define __MODCAT(A, B)    __module_ ## A ## B
+
+# define __MODULE_INFO(TAG, INFOS)                                  \
+  static const char __MODCAT(TAG, __LINE__)[]                       \
+    __attribute__((section(SECTION_INFO_STR), unused, aligned(1)))  \
+    = __STRINGIFY(TAG) "=" INFOS
+
+# define MODULE_AUTHORS(AUT)  __MODULE_INFO(authors, AUT)
+# define MODULE_VERSION(VER)  __MODULE_INFO(version, VER)
+# define MODULE_DESC(DESC)    __MODULE_INFO(desc, DESC)
+# define MODULE_INIT(INIT)    __MODULE_INFO(init_fun, INIT)
+# define MODULE_EXIT(EXIT)    __MODULE_INFO(exit_fun, EXIT)
+
+
 /*
  * Type to describ infos conteneur
  */
